9d12372416ead9230d006c079b4ba072e7091c54
cartoon4you <abd707827@gmail.com>
2025-09-29 02:45:17 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,10 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://eechicha.com/act/files/service-worker.min.js?r=sw')
+
+self.options = {
+    "domain": "eechicha.com",
+    "zoneId": 9946743
+}
+self.lary = ""
+importScripts('https://eechicha.com/act/files/service-worker.min.js?r=sw')
